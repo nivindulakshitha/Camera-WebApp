2d7b4c25a4fc24d2f74c51f6a1a3dcc602807bae
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-07-23 17:21:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const captureButton = document.getElementById("capture-button");
     const context = canvas.getContext("2d");
 
+    // Constraints to use the back camera
+    const constraints = {
+        video: {
+            facingMode: { exact: "environment" }
+        }
+    };
+    
     // Check if getUserMedia is supported
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         // Access the web camera
-        navigator.mediaDevices.getUserMedia({ video: true })
+        navigator.mediaDevices.getUserMedia(constraints)
             .then((stream) => {
                 video.srcObject = stream;
             })
@@ -17,14 +24,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Capture the image when click on the capture button
         captureButton.addEventListener("click", () => {
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
 
-            // Draw the current frame on the canvas
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        // Draw the current frame on the canvas
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-            const imageUrl = canvas.toDataURL("image/png");
-            window.open(imageUrl);
+        const imageUrl = canvas.toDataURL("image/png");
+        window.open(imageUrl);
         });
     } else {
         console.error("getUserMedia is not supported in this browser.");
